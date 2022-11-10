98dd8d61b0017da8c66711ea36589d7f0dc6571b
Àngel Àlvarez Serra <angel@nan-tic.com>
2022-11-10 09:13:57 +0100

    initial commit 041274

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# This file is part production_cost_analysis module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+from . import production
+
+
+def register():
+    Pool.register(
+        # production.AnalysisMoveStockMove,
+        production.Production,
+        production.ProductionCostAnalysis,
+        production.ProductionCostAnalysisMove,
+        production.ProductionCostAnalysisMoveDeviation,
+        production.ProductionCostAnalysisOperation,
+        production.ProductionCostAnalysisOperationDeviation,
+        module='production_cost_analysis', type_='model')
